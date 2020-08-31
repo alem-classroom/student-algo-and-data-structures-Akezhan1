6457a60a1562721bf0aa74217ecadac0773fc471
Akezhan1 <akezhka.lol@yandex.ru>
2020-08-31 12:59:18 +0600

    linear search solve

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,8 @@
 def linear_search(lst, to_find):
   if to_find in lst:
-    return lst.index(to_find)
+    i = 0
+    while i < len(lst):
+      if lst[i] == to_find:
+        return i
+      i += 1
   return -1
